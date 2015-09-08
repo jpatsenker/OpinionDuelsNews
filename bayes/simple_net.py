d4c14c6c259a6bfaacaacb31941ae2b427ba4198
Jon Patsenker <jonathan@patsenker.com>
2015-09-08 17:33:48 -0400

    fix of prev commit

--- bayes/simple_net.py
+++ bayes/simple_net.py
@@ -1,5 +1,5 @@
-import bayespy as bp
-mu = pb.nodes.Normal(0,1e-3)
+import bayespy.nodes as bp
+mu = bp.nodes.Normal(0,1e-3)
 tau = bp.nodes.Gamma(1e-3, 1e-3)
 y = bp.nodes.Normal(mu, tau, plates=(10,))
 
