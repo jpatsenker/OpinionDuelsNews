2773f7820959254a834a70c008f668f47c7cd4b1
connorfoody <connorfoody>
2015-09-09 12:54:49 -0400

    cleaned up WSJ scraper

--- scraper/src/scraper/fetcher/WSJScraper.go
+++ scraper/src/scraper/fetcher/WSJScraper.go
@@ -1,10 +1,13 @@
 package main 
 
+
 import (
 	"golang.org/x/net/html"
 	"encoding/xml"
 	"fmt"
 )
+
+// WSJ new source types
 
 type WSJArticle struct {
 	
@@ -12,84 +15,121 @@ type WSJArticle struct {
 	Link string `xml:"link"`
 	Description string `xml:"description"`
 
+	// article body
 	Data string
 }
 
-
+// TODO: add errors
 func (article WSJArticle) GetLink() string { return article.Link }
 func (article WSJArticle) GetDescription() string { return article.Description }
 func (article WSJArticle) GetTitle() string { return article.Title }
-
 func (article WSJArticle) GetData() string { return article.Data }
-func (article *WSJArticle) SetData(data string) { article.Data = data }
 
+// use ptrs for the next two because we want the article changed
+func (article *WSJArticle) SetData(data string) { article.Data = data }
 func (article *WSJArticle) DoParse(parser *html.Tokenizer) error {
-	//parser := html.NewTokenizer(body.Body)
-	i := 0
+
+	articleTagLoop:
 	for {
-		i++
 		token := parser.Next()
 		
 		switch {
-			case token == html.ErrorToken :
-				fmt.Println("found error token on first parser:", token, "\tloop is:", i)
-				fmt.Println("actual token is:", parser.Token())
-				return nil
-			case token == html.StartTagToken:
-				tmp := parser.Token()
+		case token == html.ErrorToken :
+			fmt.Println("OH NOSE!!!! ERROR before we hit the end")
+			return nil
+		case token == html.StartTagToken:
+			tmp := parser.Token()
 
-				isStartArticle := tmp.Data == "article"
-				if isStartArticle {
-					fmt.Println("found start of article")
-					// loop until we start to hit article tokens
-					for {
-						token = parser.Next()
+			isStartArticle := tmp.Data == "article"
+			if isStartArticle {
+				fmt.Println("found start of article")
+				break articleTagLoop
+			}
+		}
+	}
 
-						switch {
-							case token == html.ErrorToken:
-								return nil
-							case token == html.StartTagToken:
-								tmp = parser.Token()
-								
-								isStartArticleBody := tmp.Data == "div"
-								// loop until we are at the first paragraph of the article body
-								if isStartArticleBody {
-									isStartArticleBody = false
-									for _, attr := range tmp.Attr {
-										if attr.Key == "class" && attr.Val == "clearfix byline-wrap" {
-											isStartArticleBody = true
-											break
-										}
-									}
-									if isStartArticleBody {
-										fmt.Println("hit inside the clearfix")
-										// now loop until the body is at the first paragraph tag
-										for {
-											token = parser.Next()
-											tmp = parser.Token()
-											
-											switch {
-												case token == html.ErrorToken:
-													return nil
-												case token == html.StartTagToken:
-													isStartArticleBody = tmp.Data == "p"
-													if isStartArticleBody {
-														parser.Next()
-														tmp = parser.Token()	
-														article.SetData(tmp.Data)
-														return nil
-													}
-												case token == html.EndTagToken:
-													break
-											}
-										}
-										break // from the top for loop
+	// loop until we start to hit article tokens
+	articleStartLoop:
+	for {
+		token := parser.Next()
 
-									}
-								}
-							}
-						}
+		switch {
+		case token == html.ErrorToken:
+			return nil
+		case token == html.StartTagToken:
+			tmp := parser.Token()
+			
+			isStartArticleBody := tmp.Data == "div"
+			// loop until we are at the first paragraph of the article body
+			if isStartArticleBody {
+				isStartArticleBody = false
+				for _, attr := range tmp.Attr {
+					if attr.Key == "class" && attr.Val == "clearfix byline-wrap" {
+						isStartArticleBody = true
+						break
+					}
 				}
+				if isStartArticleBody {
+					break articleStartLoop 
+				}
+			}
+		}
+	}
+
+	// now loop until the body is at the first paragraph tag
+	articleBodyStartLoop:
+	for {
+		token := parser.Next()
+		switch {
+		case token == html.ErrorToken:
+			return nil
+		case token == html.StartTagToken:
+			tmp := parser.Token()
+			isStartArticleBody := tmp.Data == "p"
+			if isStartArticleBody {
+				break articleBodyStartLoop
+			}
+		}
+	}
+
+	// loop until article is all the way grabbed
+	addParagraph := true 
+	for {
+		token := parser.Next()
+		switch {
+		case token == html.ErrorToken:
+			return nil
+		case token == html.StartTagToken:
+			tmp := parser.Token()
+			isParagraph := tmp.Data == "p"
+			if isParagraph {
+				parser.Next()
+				tmp = parser.Token()
+				newBody := article.GetData()
+				if addParagraph {
+					newBody = newBody + "\n" + tmp.Data
+				} else{
+					addParagraph = true
+					newBody = newBody + tmp.Data
+				}
+
+				article.SetData(newBody)
+			}
+
+			isLink := tmp.Data == "a"
+			if isLink {
+				parser.Next()
+				tmp = parser.Token()
+				newBody := article.GetData() + tmp.Data
+				article.SetData(newBody)
+				// TODO: check if this is ever at the end of a paragraph....
+				addParagraph = false
+			}
+		case token == html.EndTagToken:
+			isEnd := parser.Token().Data == "div"
+			if isEnd {
+				return nil
+			}
 		}
 	}
 	return nil
@@ -102,6 +142,8 @@ type WSJRSSChannel struct {
 
 func (channel *WSJRSSChannel) GetArticle(slot int) Article {
 	if slot >= channel.GetNumArticles() {
+		// Check that the request doesn't go out of bounds
+		// TODO: errors
 		return nil
 	}
 	return &channel.Articles[slot]
@@ -115,11 +157,13 @@ type WSJRSS struct {
 	XMLName xml.Name `xml:"rss"`
 	Channel WSJRSSChannel `xml:"channel"`
 	RSSLink string
+	// TODO: actually set string to the value of the link
 }
 
 func (rss *WSJRSS) GetLink() string { return rss.RSSLink }
 
 func (rss *WSJRSS) GetChannel() RSSChannel { 
+	// return a pointer to the channel, interfaces implicitly have ptrs if they are there
 	tmp := &rss.Channel
 	return tmp 
 }
--- scraper/src/scraper/fetcher/fetchUtils.go
+++ scraper/src/scraper/fetcher/fetchUtils.go
@@ -1,16 +1,21 @@
 package main
 
 import(
-	//"golang.org/x/net/html"
+	"golang.org/x/net/html"
 )
 
 
-
+/**
+ * Types for scraping articles from a web source
+ * Most news sites seem to have rss feeds for articles
+ * RSS feeds contain channels which contain articles
+ * Use these interfaces to add a new news source
+ */
 // Generic article type
 // Gets body from an article link
 type Article interface {
 
-	//DoParse(*html.Tokenizer) error
+	DoParse(*html.Tokenizer) error
 
 	SetData(string)
 	GetData() string
@@ -21,12 +26,15 @@ type Article interface {
 }
 
 // Generic RSS feed
-// TODO: have this provide a "get new articles"
+// RSS feeds have channels
 type RSS interface {
+	// TODO: add ptr and non-ptr access to these guys
 	GetLink() string
 	GetChannel() RSSChannel
 }
 
+// Provides access to an RSS's channel, which contains the articles
+// Can't return an array here because of how interfaces are set up
 type RSSChannel interface {
 	GetArticle(int) Article
 	GetNumArticles() int
