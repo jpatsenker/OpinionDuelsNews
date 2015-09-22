3e6c7bf68df0dd0bed13cd8abb6beb7d0810ae64
Jon Patsenker <jonathan@patsenker.com>
2015-09-22 16:48:51 -0400

    Adding simple bayes go skeleton

--- bayes/simple_net.py
+++ bayes/simple_net.py
@@ -1,6 +1,1 @@
-import bayespy.nodes as bp
-mu = bp.nodes.Normal(0,1e-3)
-tau = bp.nodes.Gamma(1e-3, 1e-3)
-y = bp.nodes.Normal(mu, tau, plates=(10,))
-
-y.observe(data)
+import libpgm
---
+++ simple_bayes.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type connector struct{
+	prob int
+	nodeA string
+	nodeB string
+}
+
+
+func main(){
+
+	net = []
+	
+	net[0] = connector(nodeA: "grass_is_wet", nodeB: "rain")
+	net[1] = connector(nodeA: "grass_is_wet", nodeB: "sprinkler")
+	net[2] = connector(nodeA: "rain", nodeB: "sprinkler")
+
+
+
+
+}
