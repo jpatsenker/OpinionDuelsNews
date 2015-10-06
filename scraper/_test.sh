1d47b68ad825c974a7bf33ce3881f9abef832b8a
Samuel Johnston <samueljohnston96@gmail.com>
2015-10-05 23:06:17 -0400

    Added full and specific tests

--- scraper/_test.sh
+++ scraper/_test.sh
@@ -7,7 +7,7 @@ DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 function smoke()
 {
-	go test  scraper/config -run TestGetNEsted -v
+	go test  scraper/config -run TestGetNested -v
 	go test  scraper/config -run TestMultiRead -v
 	go test scraper/fetcher -run TestTricky -v
 	go test scraper/fetcher -run TestSchedulableRSS -v
@@ -16,11 +16,31 @@ function smoke()
 	go test scraper/scheduler -run TestScheduler -v
 	printf  "\nSmoke test run"
 }	#end of smoke
+function full()
+{
+	go test scraper/config -v
+	go test scraper/fetcher -v
+	go test scraper/scheduler -v
 
+}
+function specific()
+{	
+	for (( i = 2; i <= $#; i++ )); do
+	go test scraper/scheduler -run $i -v
+	go test  scraper/config -run $i -v
+	go test scraper/fetcher -run $i -v	
+	done
+}
 ##### Main
 case "$1" in
     "smoke" )	
 		smoke
+		;;
+	"full" )
+		full
+		;;
+	"specific" )
+		specific $@
 		;;
     * ) echo "No test input given."
 		while true; do
@@ -30,8 +50,17 @@ case "$1" in
 				echo ""
 				smoke
 				exit;;
+				full )
+				echo ""
+				full
+				exit;;
+				specific* )
+				specific $q
+				exit;;
 			[Hh] ) echo "Possible commands are:
 smoke: runs smoke test
+full: runs full test
+specific  arg: runs specified tests 
 Hh: shows commands
 Qq: quits script
 ";;
